c4edb8fdc8023f2d1750238ce07e7d57438f9aef
jangho-park-dev <qkrwkdgh0204@naver.com>
2021-10-14 17:25:44 +0900

    basic unreal project commit

---
+++ Source/PKDefense/PKDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PKDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PKDEFENSE_API APKDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
